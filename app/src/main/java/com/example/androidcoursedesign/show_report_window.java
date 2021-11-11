23f4b000c4c4f0d2174909e4599e476c7d9eb88f
WangJ <1170180928@qq.com>
2021-11-11 10:44:14 +0800

    完成截屏功能

--- app/src/main/java/com/example/androidcoursedesign/show_report_window.java
+++ app/src/main/java/com/example/androidcoursedesign/show_report_window.java
@@ -6,11 +6,15 @@ import androidx.core.content.FileProvider;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -20,6 +24,8 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class show_report_window extends AppCompatActivity {
 
@@ -36,19 +42,19 @@ public class show_report_window extends AppCompatActivity {
 //            smokePic.setRotation(90);
         }
 
-        TextView time = findViewById(R.id.time);
-        TextView location =findViewById(R.id.location);
-        TextView mode = findViewById(R.id.mode);
-        TextView status = findViewById(R.id.status);
-        TextView level = findViewById(R.id.level);
-        TextView reach = findViewById(R.id.reach);
-
-        time.append(intent.getStringExtra("time"));
-        location.append(intent.getStringExtra("location"));
-        mode.append(intent.getStringExtra("mode"));
-        status.append(intent.getStringExtra("status"));
-        level.append(intent.getStringExtra("level"));
-        reach.append(intent.getStringExtra("reach"));
+//        TextView time = findViewById(R.id.time);
+//        TextView location =findViewById(R.id.location);
+//        TextView mode = findViewById(R.id.mode);
+//        TextView status = findViewById(R.id.status);
+//        TextView level = findViewById(R.id.level);
+//        TextView reach = findViewById(R.id.reach);
+//
+//        time.append(intent.getStringExtra("time"));
+//        location.append(intent.getStringExtra("location"));
+//        mode.append(intent.getStringExtra("mode"));
+//        status.append(intent.getStringExtra("status"));
+//        level.append(intent.getStringExtra("level"));
+//        reach.append(intent.getStringExtra("reach"));
 
         Button cancel = findViewById(R.id.to_main_frame);
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -93,20 +99,73 @@ public class show_report_window extends AppCompatActivity {
 //        });
 //    }
     private void save(View thisview){
-        Intent intent= getIntent();
-        String path = intent.getStringExtra("path");
-        String fileName = intent.getStringExtra("fileName");
-        try {
-            MediaStore.Images.Media.insertImage(getContentResolver(),path,"fileName",null);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+            View view = getWindow().getDecorView();
+            //允许当前窗口保存缓存信息
+            view.setDrawingCacheEnabled(true);
+            view.buildDrawingCache();
 
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(path);
-        Uri contentUri=Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
+            //获取状态栏高度
+            Rect rect = new Rect();
+            view.getWindowVisibleDisplayFrame(rect);
+            int statusBarHeight = rect.top;
+
+            WindowManager windowManager = getWindowManager();
+
+            //获取屏幕宽和高
+            DisplayMetrics outMetrics = new DisplayMetrics();
+            windowManager.getDefaultDisplay().getMetrics(outMetrics);
+            int width = outMetrics.widthPixels;
+            int height = outMetrics.heightPixels;
+
+            //去掉状态栏
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache(), 0, statusBarHeight, width,
+                    height-statusBarHeight);
+
+            //销毁缓存信息
+            view.destroyDrawingCache();
+            view.setDrawingCacheEnabled(false);
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+
+            Toast.makeText(this, "请至权限中心打开应用相册权限", Toast.LENGTH_SHORT).show();
+        } else {
+
+            // 新建目录appDir，并把图片存到其下
+            File appDir = new File(getExternalFilesDir(null).getPath()+ "BarcodeBitmap");
+            if (!appDir.exists()) {
+
+                appDir.mkdir();
+            }
+            String fileName = System.currentTimeMillis() + ".jpg";
+            File file = new File(appDir, fileName);
+            try {
+
+                FileOutputStream fos = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.flush();
+                fos.close();
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            // 把file里面的图片插入到系统相册中
+            try {
+
+                MediaStore.Images.Media.insertImage(getContentResolver(),
+                        file.getAbsolutePath(), fileName, null);
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+            }
+
+            Toast.makeText(this, "保存"+fileName, Toast.LENGTH_SHORT).show();
+
+            // 通知相册更新
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+        }
     }
 
 }
