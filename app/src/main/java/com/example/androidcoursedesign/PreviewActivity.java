ebcc8cacc4caaf571a02496d9e2bf487de9a47a5
qiyuan han <1003111951@qq.com>
2021-11-16 22:21:13 +0800

    11.16update
    除自动识别外所有功能实现

--- app/src/main/java/com/example/androidcoursedesign/PreviewActivity.java
+++ app/src/main/java/com/example/androidcoursedesign/PreviewActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -18,9 +19,15 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URI;
 
 public class PreviewActivity extends AppCompatActivity {
     private final String curAct= "PreviewActivity";
+    private Uri uri=null;
+    private Uri uri3=null;
+    private String picpath;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,18 +40,17 @@ public class PreviewActivity extends AppCompatActivity {
         tookPic.setImageURI(Uri.fromFile(file));
         //照片也进行一个旋转
         tookPic.setRotation(90);
-
+        //保存到图库
+        saveImageToGallery(PreviewActivity.this,file,fileName);
 
         Button confirm =findViewById(R.id.pic_confirm);
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //保存到图库
-                saveImageToGallery(PreviewActivity.this,file,fileName);
                 //切换
                 Intent it0 = new Intent(PreviewActivity.this, ReportGenActivity.class);
-                it0.putExtra("path", path);
-                it0.putExtra("fileName", fileName);
+                it0.putExtra("path", picpath);
+                it0.putExtra("imageUri", uri.toString());
                 //0默认晴天 1是阴天
                 it0.putExtra("pattern",0);
                 startActivity(it0);
@@ -55,10 +61,10 @@ public class PreviewActivity extends AppCompatActivity {
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //切换
                 Intent it1 = new Intent(PreviewActivity.this, EditActivity.class);
-                it1.putExtra("path", path);
-                it1.putExtra("fileName", fileName);
+                it1.putExtra("path", picpath);
+                it1.putExtra("imageUri", uri.toString());
                 startActivity(it1);
 
                 //TODO :
@@ -81,12 +87,34 @@ public class PreviewActivity extends AppCompatActivity {
         });
 
     }
+    public String getRealPathFromURI(Uri contentUri) {//通过本地路经 content://得到URI路径
+        Cursor cursor = null;
+        String locationPath = null ;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor= getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            locationPath = cursor.getString(column_index);
+        } catch (Exception e) {
+        }finally{
+            if(cursor != null)
+            {
+                cursor.close();
+            }
+        }
+        return locationPath;
+    }
+
     public void saveImageToGallery(Context context, File file, String fileName) {
         // 把文件插入到系统图库
         try {
-            MediaStore.Images.Media.insertImage(context.getContentResolver(),
+            String bitpath = MediaStore.Images.Media.insertImage(context.getContentResolver(),
                     file.getAbsolutePath(), fileName, null);
                     Toast.makeText(context, "保存照片成功", Toast.LENGTH_SHORT).show();
+            uri=Uri.parse(bitpath);
+            //Toast.makeText(PreviewActivity.this, uri3.toString(), Toast.LENGTH_LONG).show();
+            picpath=getRealPathFromURI(uri);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             Toast.makeText(context, "保存照片失败", Toast.LENGTH_SHORT).show();
@@ -95,4 +123,6 @@ public class PreviewActivity extends AppCompatActivity {
         // 通知图库更新
         context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(new File(file.getPath()))));
     }
+
+
 }
